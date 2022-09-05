57a3d8153f2290b15529815e5bd75d725b37355f
Camilo <camiloa.ortizm@gmail.com>
2022-09-04 23:12:06 -0300

    descarga de archivos

--- biblioteca-src/descarga.php
+++ biblioteca-src/descarga.php
@@ -2,14 +2,17 @@
 
 
     $archivo = $_REQUEST['file'];
-    $ruta_archivo = 'static/img/'. $archivo;
+    $ruta_archivo = '/static/img/'. $archivo;
     $size =filesize($ruta_archivo);
 
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.$archivo.'"');
     header('Expires: 0');
-    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-    header("Content-Disposition: attachment; filename=".$archivo."");
-    header('Content-Transfer-Encoding: binary');
-    header('Content-Length:' . $size);
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . $size);
+    readfile($$ruta_archivo);
 
     readfile($ruta_archivo);
 
